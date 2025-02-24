e8ba32fb329ca5684413751ba4ec6b25b3685d43
huma0001 <huma0001@stud.kea.dk>
2025-02-24 18:57:39 +0100

    dsads

---
+++ src/main/java/com/example/faaaak/FaaaakApplication.java
@@ -0,0 +1,13 @@
+package com.example.faaaak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaaaakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaaaakApplication.class, args);
+    }
+
+}
